14b24e680153162305fd34f2c68a09e9c569cd97
cepthomas <cepthomas@gmail.com>
2024-10-08 17:00:57 -0500

    Test updates

--- tests/test_signet.py
+++ tests/test_signet.py
@@ -4,10 +4,12 @@ import traceback
 import unittest
 from unittest.mock import MagicMock
 
+
 # Set up the sublime emulation environment.
 import emu_sublime_api as emu
 
 # Import the code under test.
+import sbot_signet
 import sbot_common as sc
 
 
